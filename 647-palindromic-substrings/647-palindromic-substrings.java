d2c026a2ff0cd908b492c405d9f12ebe25eccc82
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2022-03-22 13:41:17 +0530

    Time: 530 ms (11.45%), Space: 118.1 MB (5.47%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,6 +1,9 @@
 class Solution {
     
     public int countSubstrings(String s) {
+        if(s.length()==0){
+            return 0;
+        }
         int c = 0;
         for(int i=0;i<s.length();i++){
             for(int j=i+1;j<=s.length();j++){
