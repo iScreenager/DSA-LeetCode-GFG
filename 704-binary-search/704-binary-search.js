a8154b52a7af9913c3420de8b48de3c2788f537c
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2022-04-13 18:27:41 +0530

    Time: 69 ms (80.68%), Space: 45.2 MB (52.92%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -1,20 +1,21 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        
-        int s = 0;
-        int e = nums.length-1;
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]<target){
-                s = mid+1;
-            }else{
-                e = mid-1;
-            }
-           
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let s = 0;
+    let e = nums.length -1;
+    while(s <= e){
+        const mid = Math.floor(s+(e-s)/2);
+        if(target== nums[mid]){
+            return mid;
+        }else if(target<nums[mid]){
+            e = mid-1;
+        }else{
+            s = mid+1;
         }
-        return -1;
-        
     }
-}
+    return -1;
+    
+};
