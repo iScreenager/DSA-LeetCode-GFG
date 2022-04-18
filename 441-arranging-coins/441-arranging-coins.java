a352f35f45a987f6df864c17bb5206fbe0c85029
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2022-04-18 16:59:02 +0530

    Time: 2 ms (88.23%), Space: 39.9 MB (81.08%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.java
+++ 441-arranging-coins/441-arranging-coins.java
@@ -3,13 +3,22 @@ class Solution {
         if(n==1){
             return 1;
         }
-        int coin = n;
-       int i = 1;
-        while(coin>0){
-            i++;
-            coin = coin-i;
-        }
-        return i-1;
-        
+      long s = 0;
+            long e = n;
+            
+            while(s <= e){
+                    long m = s+(e-s)/2;
+                    long currentCoin = m*(m+1)/2;
+                    
+                    if(n==currentCoin ){
+                            return (int)m;
+                    }else if(n < currentCoin){
+                            e = m-1;
+                    }else{
+                            s = m+1;
+                    }
+            }
+            return (int)e;
     }
+        
 }
