58a68a21277dc3d2399ac6f48ac3594f999d1652
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2022-06-15 13:57:18 +0530

    Time: 1 ms (99.89%), Space: 44.3 MB (36.11%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -3,26 +3,25 @@ class Solution {
             Arrays.sort(nums);
             List<List<Integer>> outer = new ArrayList<>();
             outer.add(new ArrayList<>());
+            int start=0;
+            int end = 0;
             
-            int s = 0;
-            int e = 0;
-            
-            for(int i=0;i<nums.length;i++){
-                    s=0;
-                    if(i>0 && nums[i] == nums[i-1]){
-                            s= e+1;
-                    }
-                    
-                    e= outer.size()-1;
-                    int n = outer.size();
-                    
-                    for(int j=s;j<n;j++){
-                            List<Integer> internal = new ArrayList<>(outer.get(j));
-                            internal.add(nums[i]);
-                            outer.add(internal);
-                    }
-            }
+           for(int i=0;i<nums.length;i++){
+                   start = 0;
+                   if(i>0 && nums[i]==nums[i-1]){
+                           start=end+1;
+                   }
+                   
+                   end = outer.size()-1;
+                   int n = outer.size();
+                   
+                   for(int j=start;j<n;j++){
+                           List<Integer> internal = new ArrayList<>(outer.get(j));
+                           internal.add(nums[i]);
+                           outer.add(internal);
+                   }
+           }
             return outer;
-        
+            
     }
 }
