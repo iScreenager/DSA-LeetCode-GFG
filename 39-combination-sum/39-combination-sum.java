baa4e0e4ddca85cd071aef9ca4d6bcdbab88c889
Vaishnavi <87562957+iScreenager@users.noreply.github.com>
2022-07-29 11:33:48 +0530

    Time: 3 ms (95.20%), Space: 45.6 MB (37.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,23 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> outer = new ArrayList<>();
-            helperCombinationSum(0,candidates,target,outer,new ArrayList<>());
+            List<List<Integer>> outer = new ArrayList<>();
+             
+            helper(0,candidates,target,outer,new ArrayList<>());
             return outer;
+        
     }
-        static void  helperCombinationSum(int idx,int[] candidates,int target,List<List<Integer>> outer,List<Integer> inner){
-                if(idx == candidates.length){
-                if(target == 0){
-                        outer.add(new ArrayList<>(inner));
-                       
-                }
-                         return;
+        public void helper(int ind,int[] candidates,int target,List<List<Integer>> outer,            List<Integer> inner){
+                if(ind == candidates.length){
+                        if(target == 0){
+                                outer.add( new ArrayList<>(inner));
+                        }
+                        return;
                 }
                 
-                if(candidates[idx] <= target){
-                        inner.add(candidates[idx]);
-                        helperCombinationSum(idx,candidates,target-candidates[idx],outer,inner);
+                if(candidates[ind] <= target){
+                        inner.add(candidates[ind]);
+                        helper(ind,candidates,target-candidates[ind],outer,inner);
                         inner.remove(inner.size()-1);
                 }
-                 helperCombinationSum(idx+1,candidates,target,outer,inner);
+                helper(ind+1,candidates,target,outer,inner);
         }
 }
